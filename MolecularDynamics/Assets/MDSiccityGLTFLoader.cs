514340044df9a12244b40876414d0cc1e57143bc
Brandon Dang <bcdang2@illinois.edu>
2021-08-06 21:18:40 -0500

    WIP: installed SiccityGLTF and started integrating

---
+++ MolecularDynamics/Assets/MDSiccityGLTFLoader.cs
@@ -0,0 +1,20 @@
+﻿using Siccity.GLTFUtility;
+using UnityEngine;
+
+namespace Komodo.AssetImport 
+{
+    public class MDSiccityGLTFLoader : ModelDownloaderAndLoader
+    {
+        public override void LoadLocalFile(string localFilename, System.Action<GameObject> callback)
+        {
+            if (callback == null)
+            {
+                throw new System.Exception("callback was null");
+            }
+
+            callback(Importer.LoadFromFile(localFilename));
+        }
+    }
+}
+
+//https://cdn.jsdelivr.net/ghparseccentric/komodo-test-models/pyrite-conversion-VMDScene001.glb
